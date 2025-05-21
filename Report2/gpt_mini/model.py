fca170497b0a1d2b05a917483e48eae24337cdd1
Rob Rohan <robrohan@Robs-MacBook-Pro.local>
2025-05-22 06:52:52 +1200

    Maybe 0 is ok

--- Report2/gpt_mini/model.py
+++ Report2/gpt_mini/model.py
@@ -354,7 +354,6 @@ class GPT(nn.Module):
                 logits = logits[:, -1, :] / temperature
             except Exception as ex:
                 print(ex, logits)
-                raise ex
             # optionally crop the logits to only the top k options
             if top_k is not None:
                 v, _ = torch.topk(logits, top_k)
