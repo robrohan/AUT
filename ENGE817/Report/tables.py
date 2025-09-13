4eca7a32fa6d87b160093a789c3ce60aa96cad02
Rob Rohan <rob@therohans.com>
2025-09-13 21:09:10 +1200

    Playing with score

---
+++ ENGE817/Report/tables.py
@@ -0,0 +1,44 @@
+classes = [
+    "Intellectual Developmental Disorders",                        # 0
+    "Communication Disorders",
+    "Autism Spectrum Disorder",
+    "Attention-Deficit/Hyperactivity Disorder",
+    "Specific Learning Disorder",
+    "Motor Disorders",
+    "Other Neurodevelo mental Disorders p",
+    "Schizophrenia Spectrum and Other Psychotic Disorders",
+    "Catatonia",
+    "Bipolar and Related Disorders",
+    "Depressive Disorders",                                        # 10
+    "Anxiety Disorders",
+    "Obsessive-Compulsive and Related Disorders",
+    "Trauma- and Stressor-Related Disorders",
+    "Dissociative Disorders",
+    "Somatic Symptom and Related Disorders",
+    "Feeding and Eating Disorders",
+    "Elimination Disorders",
+    "Sleep-Wake Disorders",
+    "Breathing-Related Sleep Disorders",
+    "Parasomnias",                                                # 20
+    "Sexual Dysfunctions",
+    "Gender Dysphoria",
+    "Disruptive, Impulse-Control, and Conduct Disorders",
+    "Neurocognitive Disorders",
+    "Personality Disorders",
+    "Cluster A Personality Disorders",
+    "Cluster B Personality Disorders",
+    "Cluster C Personalit Disorders y",
+    "Other Personality Disorders",
+    "Paraphilic Disorders",                                       # 30
+    "Other Mental Disorders and Additional Codes",
+    "Additional Codes",
+    "Medication-Induced Movement Disorders and Other Adverse Effects of Medication",
+    "Other Conditions That May Be a Focus of Clinical Attention", # 34
+]
+
+schema_dicts = [
+    { "name": "Distilbert", "url": "http://t2v-transformers:8080", "schema": "DSMDistilbert" },
+    { "name": "Roberta", "url": "http://t2v-transformers-drobert:8080", "schema": "DSMRoberta" },
+    { "name": "Biobert", "url": "http://t2v-transformers-biobert:8080", "schema": "DSMBiobert" },
+    { "name": "Clinicalbert", "url": "http://t2v-transformers-clicbert:8080", "schema": "DSMClinicalbert" },
+]
